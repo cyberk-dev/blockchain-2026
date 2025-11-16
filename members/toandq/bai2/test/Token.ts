2f7797bc3601c171347c7ce24cbdc5487d770e52
Treacle <68235687+bangbu10820@users.noreply.github.com>
2025-11-16 13:19:53 +0700

    chore: update test

--- members/toandq/bai2/test/Token.ts
+++ members/toandq/bai2/test/Token.ts
@@ -1,3 +1,4 @@
+import assert from "node:assert/strict";
 import { describe, it } from "node:test";
 
 import { network } from "hardhat";
@@ -9,14 +10,28 @@ describe("Token", async function () {
   const publicClient = await viem.getPublicClient();
 
   it("Deploy ignition", async function () {
+    const name = "CBK";
+    const symbol = "ABC";
+    const initialSupply = parseUnits("100", 18);
+
     const { token } = await ignition.deploy(TokenModule, {
       parameters: {
         TokenModule: {
-          name: "CBK",
-          symbol: "ABC",
-          initialSupply: parseUnits("100", 18),
+          name,
+          symbol,
+          initialSupply,
         },
       },
     });
+
+    // Read token information
+    const tokenName = await token.read.name();
+    const tokenSymbol = await token.read.symbol();
+    const tokenTotalSupply = await token.read.totalSupply();
+
+    // Match with input parameters
+    assert.equal(tokenName, name, "Token name should match");
+    assert.equal(tokenSymbol, symbol, "Token symbol should match");
+    assert.equal(tokenTotalSupply, initialSupply, "Token total supply should match");
   });
 });
