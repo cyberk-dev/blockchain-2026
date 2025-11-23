d337b1b665d5e6e1037e7abe2f5076fcc5e55264
maivl-cyberk <maivl@cyberk.io>
2025-11-23 23:25:50 +0700

    mai-04: price

---
+++ members/maivl/04/contracts/Token.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+import "hardhat/console.sol";
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {
+    ReentrancyGuard
+} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {FullMath} from "./FullMath.sol";
+
+contract Token is ERC20, Ownable, ReentrancyGuard {
+    using FullMath for uint256;
+    uint256 endTime;
+    uint256 public totalWeiSold;
+    uint256 public a;
+    uint256 public b;
+    uint256 public scale;
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint256 _initialSupply,
+        uint256 _endTime,
+        uint256 _a,
+        uint256 _b,
+        uint256 _scale
+    ) ERC20(_name, _symbol) Ownable(msg.sender) {
+        a = _a;
+        b = _b;
+        scale = _scale;
+        _mint(msg.sender, _initialSupply);
+
+        endTime = _endTime;
+        totalWeiSold = _initialSupply;
+    }
+
+    error InsufficientFunds();
+    error InvalidAmount();
+    error SaleEnded();
+
+    function getCost(uint256 s, uint256 m) public view returns (uint256) {
+        uint256 part1 = a * (m * s);
+        uint256 part2 = a * ((m * (m + 1)) / 2);
+        uint256 part3 = b * m;
+
+        uint256 sum = part1 + part2 + part3;
+        uint256 result = sum.mulDiv(1, scale);
+        return result;
+    }
+
+    modifier saleActive() {
+        if (block.timestamp > endTime) revert SaleEnded();
+        _;
+    }
+
+    function buyToken(
+        uint256 _amount
+    ) external payable saleActive nonReentrant {
+        if (_amount == 0) revert InvalidAmount();
+        uint256 cost = getCost(totalWeiSold, _amount);
+        if (msg.value < cost) revert InsufficientFunds();
+
+        // Mint token
+        _mint(msg.sender, _amount);
+
+        totalWeiSold += _amount;
+
+        if (msg.value > cost) {
+            (bool ok, ) = payable(msg.sender).call{value: msg.value - cost}("");
+            require(ok, "Refund failed");
+        }
+    }
+
+    function decimals() public view override returns (uint8) {
+        return 18;
+    }
+}
---
+++ members/maivl/04/test/Token.ts
@@ -0,0 +1,95 @@
+import assert from "node:assert/strict";
+import { describe, it } from "node:test";
+import { parseUnits } from "viem";
+
+import { network } from "hardhat";
+import TokenModule from "../ignition/modules/Token.js";
+import { NetworkConnection } from "hardhat/types/network";
+
+async function deploy(connection: NetworkConnection) {
+    const { viem, ignition } = await network.connect();
+    const publicClient = await viem.getPublicClient();
+
+    const blockNumber = await publicClient.getBlockNumber();
+    const block = await publicClient.getBlock({ blockNumber });
+    const now = Number(block.timestamp);
+    const endTimeFuture = now + 3600;
+
+    const deployment = await ignition.deploy(TokenModule, {
+        parameters: {
+            TokenModule: {
+                name: "Cyberk",
+                symbol: "CBK",
+                initialSupply: 0n,
+                a: 1n,
+                b: 12n,
+                scale: 10n ** 22n,
+                endTime: BigInt(endTimeFuture)
+            },
+        },
+    });
+
+    const tokenAddress = (deployment.token as { address: `0x${string}` }).address;
+    const token = await viem.getContractAt("Token", tokenAddress);
+
+    return { viem, publicClient, token };
+}
+
+function manualCost(a: bigint, b: bigint, SCALE: bigint, s: bigint, m: bigint) {
+    const numerator = a * (m * s) + a * ((m * (m + 1n)) / 2n) + b * m;
+    return numerator / SCALE;
+}
+
+describe("Token Pricing & Buying", async function () {
+    it("Price Check – First Token", async function () {
+        const { networkHelpers } = await network.connect();
+        const { token } = await networkHelpers.loadFixture(deploy);
+
+        const oneToken = 10n ** 18n;
+        const cost = await token.read.getCost([0n, oneToken]);
+        const expected = manualCost(1n, 12n, 10n ** 22n, 0n, oneToken);
+
+        assert.equal(cost, expected, "First token price mismatch");
+    });
+
+    it("Price Check – Next 10 Tokens", async function () {
+        const { networkHelpers } = await network.connect();
+        const { token } = await networkHelpers.loadFixture(deploy);
+
+        const oneToken = 10n ** 18n;
+
+        const tenTokens = 10n * (10n ** 18n);
+        const cost = await token.read.getCost([oneToken, tenTokens]);
+        const expected = manualCost(1n, 12n, 10n ** 22n, oneToken, tenTokens);
+
+        assert.equal(cost, expected, "10-token batch price mismatch");
+    });
+
+    it("Formula Check – First Token + Next 10 Tokens = 11 First Tokens", async function () {
+        const { networkHelpers } = await network.connect();
+        const { token } = await networkHelpers.loadFixture(deploy);
+
+        const oneToken = 10n ** 18n;
+        const firstTokenCost = await token.read.getCost([0n, oneToken]);
+
+        const tenTokens = 10n * (10n ** 18n);
+        const nextTenTokensCost = await token.read.getCost([oneToken, tenTokens]);
+
+        const cost = firstTokenCost + nextTenTokensCost;
+
+        const elevenTokens = 11n * (10n ** 18n);
+        const expected = manualCost(1n, 12n, 10n ** 22n, 0n, elevenTokens);
+
+        assert.equal(cost, expected, "Formula is wrong");
+    });
+
+    it("Purchase Execution – buyToken succeeds", async function () {
+      const { networkHelpers } = await network.connect();
+      const { token } = await networkHelpers.loadFixture(deploy);
+
+      const amount = 5n * (10n ** 18n);
+      const cost = manualCost(1n, 12n, 10n ** 22n, 0n, amount);
+
+      await token.write.buyToken([amount], { value: cost });
+    });
+});
