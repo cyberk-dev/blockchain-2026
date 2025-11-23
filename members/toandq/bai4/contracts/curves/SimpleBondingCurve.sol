e399a44c0c235f74fcb3e9b44f2341e9f0ebeb04
Bang Bu <68235687+bangbu10820@users.noreply.github.com>
2025-11-23 23:23:23 +0700

    Toandq 4 - Token simple bonding curve with erc20 payment  (#15)
    * init project
    
    * feat: update contract and testcase with viem test

---
+++ members/toandq/bai4/contracts/curves/SimpleBondingCurve.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {FullMath} from "../FullMath.sol";
+
+library SimpleBondingCurve {
+    using FullMath for uint256;
+
+    // https://www.wolframalpha.com/input?i2d=true&i=Sum%5BDivide%5Bx%2Ca%5D+%2B+b%2C%7Bx%2Cs%2B1%2Cs%2Bm%7D%5D
+    function calculatePrice(
+        uint256 supply,
+        uint256 amount,
+        uint256 slope,
+        uint256 intercept
+    ) internal pure returns (uint256) {
+        return
+            amount.mulDivRoundingUp(
+                2 * slope * intercept + amount + 2 * supply + 1,
+                2 * slope
+            );
+    }
+}
