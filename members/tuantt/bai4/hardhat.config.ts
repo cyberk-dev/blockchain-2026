0f2691c8b033f2baa41a5f45de11d4262037226d
tuantt-cyberk <tuantt@cyberk.io>
2025-11-23 21:40:15 +0700

    tuantt bai 4

---
+++ members/tuantt/bai4/hardhat.config.ts
@@ -0,0 +1,63 @@
+import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
+import hardhatVerify from "@nomicfoundation/hardhat-verify";
+import { configVariable, defineConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-viem-assertions";
+import "./plugins/type-extensions.js";
+import hardhatViemAssertions from "@nomicfoundation/hardhat-viem-assertions";
+
+export default defineConfig({
+  plugins: [
+    hardhatToolboxViemPlugin,
+    hardhatVerify,
+    hardhatViemAssertions,
+    {
+      id: "hardhat-viem-assertions-extended",
+      dependencies: () => [],
+      hookHandlers: {
+        // Load the plugin logic when the network starts
+        network: () => import("./plugins/viem-test.js"),
+      },
+    },
+  ],
+  solidity: {
+    profiles: {
+      default: {
+        version: "0.8.28",
+      },
+      production: {
+        version: "0.8.28",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    },
+    npmFilesToBuild: [
+      "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol",
+      "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol",
+    ],
+  },
+  networks: {
+    hardhatMainnet: {
+      type: "edr-simulated",
+      chainType: "l1",
+    },
+    hardhatOp: {
+      type: "edr-simulated",
+      chainType: "op",
+    },
+    sepolia: {
+      type: "http",
+      chainType: "l1",
+      url: configVariable("SEPOLIA_RPC_URL"),
+      accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+    },
+  },
+  verify: {
+    etherscan: {
+      apiKey: "",
+    },
+  },
+});
