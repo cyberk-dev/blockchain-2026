1487b1c72d3ea99c2088632cfd7ee43becb74d55
tuantt-cyberk <tuantt@cyberk.io>
2025-11-30 20:21:06 +0700

    feat: bai 6

---
+++ members/tuantt/bai6/contracts/interfaces/IToken.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+interface IToken {
+    function initialize(string memory name, string memory symbol, address usdt_address) external;
+    function owner() external view returns (address);
+    function buyTokens(uint256 amount) external payable;
+}
