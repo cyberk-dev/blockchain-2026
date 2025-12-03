c8cf4e964463558a963fbf3746b2deec20b78d58
Treacle <68235687+bangbu10820@users.noreply.github.com>
2025-12-04 00:52:16 +0700

    feat: LP token contract

--- members/toandq/bai7/contracts/LPToken.sol
+++ members/toandq/bai7/contracts/LPToken.sol
@@ -1,13 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
-import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {FullMath} from "./FullMath.sol";
 
-contract LPToken is ERC20 {
+contract LPToken is ERC20, Ownable {
     using Math for uint256;
     using SafeERC20 for IERC20;
 
@@ -34,13 +35,17 @@ contract LPToken is ERC20 {
 
     error InsufficientLiquidity();
     error InsufficientLiquidityBurned();
+    error InsufficientAmountOut();
 
-    constructor(address _token0, address _token1) ERC20("LPToken", "LP") {
+    constructor(
+        address _token0,
+        address _token1
+    ) ERC20("LPToken", "LP") Ownable(msg.sender) {
         token0 = _token0;
         token1 = _token1;
     }
 
-    function mintLiquidity(address to) external {
+    function mintLiquidity(address to) external onlyOwner {
         uint256 balance0 = IERC20(token0).balanceOf(msg.sender);
         uint256 balance1 = IERC20(token1).balanceOf(msg.sender);
 
@@ -74,7 +79,7 @@ contract LPToken is ERC20 {
         emit LiquidityAdded(to, amount0, amount1, liquidity);
     }
 
-    function burnLiquidity(address to) external {
+    function burnLiquidity(address to) external onlyOwner {
         uint balance0 = IERC20(token0).balanceOf(address(this));
         uint balance1 = IERC20(token1).balanceOf(address(this));
         uint liquidity = balanceOf(address(this)); // User need to send lp to pair first to burn
@@ -98,5 +103,111 @@ contract LPToken is ERC20 {
         }
 
         emit LiquidityRemoved(to, amount0, amount1, liquidity);
+    }
+
+    function swapExactOut(
+        address tokenIn,
+        address tokenOut,
+        uint256 amountIn,
+        uint256 amountOutMin
+    ) external onlyOwner {
+        uint256 reserveIn = tokenIn == token0 ? reserve0 : reserve1;
+        uint256 reserveOut = tokenIn == token0 ? reserve1 : reserve0;
+
+        if (amountOutMin > reserveOut) revert InsufficientAmountOut();
+
+        amountIn = _getAmountIn(amountOutMin, reserveIn, reserveOut);
+
+        IERC20(tokenIn).safeTransferFrom(msg.sender, address(this), amountIn);
+        IERC20(tokenOut).safeTransfer(msg.sender, amountOutMin);
+
+        uint256 balance0 = IERC20(token0).balanceOf(address(this));
+        uint256 balance1 = IERC20(token1).balanceOf(address(this));
+
+        unchecked {
+            reserve0 = balance0;
+            reserve1 = balance1;
+            kLast = reserve0.saturatingMul(reserve1);
+        }
+    }
+
+    function swapExactIn(
+        address tokenIn,
+        address tokenOut,
+        uint256 amountOut,
+        uint256 amountInMax
+    ) external onlyOwner {
+        uint256 reserveIn = tokenIn == token0 ? reserve0 : reserve1;
+        uint256 reserveOut = tokenIn == token0 ? reserve1 : reserve0;
+
+        amountOut = _getAmountOut(amountInMax, reserveIn, reserveOut);
+
+        if (amountOut > reserveOut) revert InsufficientAmountOut();
+
+        IERC20(tokenIn).safeTransferFrom(
+            msg.sender,
+            address(this),
+            amountInMax
+        );
+        IERC20(tokenOut).safeTransfer(msg.sender, amountOut);
+
+        uint256 balance0 = IERC20(token0).balanceOf(address(this));
+        uint256 balance1 = IERC20(token1).balanceOf(address(this));
+
+        unchecked {
+            reserve0 = balance0;
+            reserve1 = balance1;
+            kLast = reserve0.saturatingMul(reserve1);
+        }
+    }
+
+    function getAmountIn(
+        address tokenIn,
+        address tokenOut,
+        uint256 amountOutMin
+    ) external view returns (uint256 amountIn) {
+        uint256 reserveIn = tokenIn == token0 ? reserve0 : reserve1;
+        uint256 reserveOut = tokenIn == token0 ? reserve1 : reserve0;
+        amountIn = _getAmountIn(amountOutMin, reserveIn, reserveOut);
+    }
+
+    function getAmountOut(
+        address tokenIn,
+        address tokenOut,
+        uint256 amountInMax
+    ) external view returns (uint256 amountOut) {
+        uint256 reserveIn = tokenIn == token0 ? reserve0 : reserve1;
+        uint256 reserveOut = tokenIn == token0 ? reserve1 : reserve0;
+        amountOut = _getAmountOut(amountInMax, reserveIn, reserveOut);
+    }
+
+    function _getAmountIn(
+        uint256 amountOut,
+        uint256 reserveIn,
+        uint256 reserveOut
+    ) internal pure returns (uint256 amountIn) {
+        // deltaX=deltaY*reserveIn/(reserveOut-deltaY)
+        // amountIn=amountOut*reserveIn/(reserveOut-amountOut)
+        // Fee is 0.3%
+        uint numerator = amountOut.saturatingMul(reserveIn).saturatingMul(1000);
+        uint denominator = reserveOut.saturatingSub(amountOut).saturatingMul(
+            997
+        );
+        amountIn = FullMath.mulDivRoundingUp(numerator, 1, denominator);
+    }
+
+    function _getAmountOut(
+        uint256 amountIn,
+        uint256 reserveIn,
+        uint256 reserveOut
+    ) internal pure returns (uint256 amountOut) {
+        // deltaY=deltaX*reserveOut/(reserveIn+deltaX)
+        // Fee is 0.3%
+        uint amountInWithFee = amountIn.saturatingMul(997);
+        uint numerator = amountInWithFee.saturatingMul(reserveOut);
+        uint denominator = reserveIn
+            .saturatingAdd(amountInWithFee)
+            .saturatingMul(1000);
+        amountOut = FullMath.mulDivRoundingUp(numerator, 1, denominator);
     }
 }
