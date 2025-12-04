9f0e8ebe483ce1dee1cadf6009c64f63bf4f3e4c
duchmio <duchm@cyberk.io>
2025-12-04 13:13:29 +0700

    feat(duchm/07): create add liquidity test

--- members/duchm/07/test/LP.ts
+++ members/duchm/07/test/LP.ts
@@ -32,6 +32,16 @@ async function setup() {
   return ctx;
 }
 
+const mintAndApprove = async (token: any, account: any, spender: string, amount: bigint) => {
+  await token.write.mint([account.address, amount]);
+  await token.write.approve([spender, amount], { account });
+};
+
+const getPair = async (viem: any, factory: any, t0: string, t1: string, acc: any) => {
+  const pairAddr = await factory.read.lpPairs([t0, t1]);
+  return await viem.getContractAt("LPToken", pairAddr, { client: { wallet: acc } });
+};
+
 describe("TokenFactory", async function () {
   it("Should deploy token", async function () {
     const { lpFactory } = await setup();
@@ -49,5 +59,20 @@ describe("TokenFactory", async function () {
     assert.notEqual(pairAddr, "0x0000000000000000000000000000000000000000");
     const pairAddrReverse = await lpFactory.read.lpPairs([token1.address, token0.address]);
     assert.equal(pairAddr, pairAddrReverse);
+  });
+
+  it("Should add liquidity", async function () {
+    const { lpFactory, token0, token1, owner, viem } = await setup();
+    await lpFactory.write.createLP([token0.address, token1.address]);
+    const pair = await getPair(viem, lpFactory, token0.address, token1.address, owner);
+    await mintAndApprove(token0, owner.account, pair.address, parseEther("10000"));
+    await mintAndApprove(token1, owner.account, pair.address, parseEther("10000"));
+    await pair.write.addLiquidity([parseEther("100"), parseEther("400")]);
+    const balance1 = await pair.read.balanceOf([owner.account.address]);
+    assert.equal(balance1, parseEther("200"));
+
+    await pair.write.addLiquidity([parseEther("500"), parseEther("500")]);
+    const balance2 = await pair.read.balanceOf([owner.account.address]);
+    assert.equal(balance2, parseEther("450"));
   });
 })
