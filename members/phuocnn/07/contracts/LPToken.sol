6c4436395258f1fa962753d04e5fec94de041d3e
trungtran <trungtran@cyberk.io>
2025-12-18 14:28:07 +0700

    phuocnn: uniswap-v2

--- members/phuocnn/07/contracts/LPToken.sol
+++ members/phuocnn/07/contracts/LPToken.sol
@@ -8,6 +8,8 @@ import "./FullMath.sol";
 
 contract LPToken is ERC20 {
     using SafeERC20 for IERC20;
+    using FullMath for uint256;
+
     address public token0;
     address public token1;
 
@@ -22,6 +24,13 @@ contract LPToken is ERC20 {
     error InvalidToken();
 
     constructor() ERC20("LPToken", "LP") {}
+
+    // 100 token A/ 
+    // 1 token B
+
+    // 100 token A (có cả fee)
+    // => 99,7 token A
+    // ? token B
 
     function initialize(
         address _token0,
@@ -134,8 +143,7 @@ contract LPToken is ERC20 {
     function swap_amount_in(
         uint256 amountIn,
         address tokenIn,
-        uint256 amountOutMin,
-        address to
+        uint256 amountOutMin
     ) external returns (uint256 amountOut) {
         if (amountIn == 0) revert InvalidInput();
         if (tokenIn != token0 && tokenIn != token1) revert InvalidInput();
@@ -151,13 +159,15 @@ contract LPToken is ERC20 {
             reserveOut = reserve0;
         }
 
-        amountOut = FullMath.mulDiv(amountIn, reserveOut, reserveIn + amountIn);
+        uint256 amountInReal = amountIn.mulDiv(997, 1000);
+
+        amountOut = FullMath.mulDiv(amountInReal, reserveOut, reserveIn + amountInReal);
         if (amountOut < amountOutMin) revert InsufficientOutput();
 
         address tokenOut = tokenIn == token0 ? token1 : token0;
 
         IERC20(tokenIn).safeTransferFrom(msg.sender, address(this), amountIn);
-        IERC20(tokenOut).safeTransfer(to, amountOut);
+        IERC20(tokenOut).safeTransfer(msg.sender, amountOut);
 
         if (tokenIn == token0) {
             reserve0 += amountIn;
@@ -171,8 +181,7 @@ contract LPToken is ERC20 {
     function swap_exact_out(
         uint256 amountOut,
         uint256 amountInMax,
-        address tokenOut,
-        address to
+        address tokenOut
     ) external returns (uint256 amountIn) {
         if (amountOut == 0) revert InvalidInput();
         if (tokenOut != token0 && tokenOut != token1) revert InvalidToken();
@@ -190,10 +199,12 @@ contract LPToken is ERC20 {
 
         if (reserveOut < amountOut) revert InsufficientOutput();
 
+        // amountInReal = amountInWithFee * 997 / 1000
+
         amountIn = FullMath.mulDivRoundingUp(
             amountOut,
-            reserveIn,
-            reserveOut - amountOut
+            reserveIn * 1000,
+            (reserveOut - amountOut) * 997
         );
 
         address tokenIn = tokenOut == token0 ? token1 : token0;
@@ -204,7 +215,7 @@ contract LPToken is ERC20 {
             amountInMax
         );
 
-        IERC20(tokenOut).safeTransfer(to, amountOut);
+        IERC20(tokenOut).safeTransfer(msg.sender, amountOut);
 
         if (tokenOut == token0) {
             reserve0 -= amountOut;
@@ -215,7 +226,7 @@ contract LPToken is ERC20 {
         }
 
         if (amountIn > amountInMax) {
-            IERC20(tokenIn).safeTransfer(to, amountInMax - amountIn);
+            IERC20(tokenIn).safeTransfer(msg.sender, amountInMax - amountIn);
         }
     }
 }
--- members/phuocnn/07/test/LPToken.ts
+++ members/phuocnn/07/test/LPToken.ts
@@ -1,10 +1,11 @@
-import assert from "node:assert/strict";
-import { describe, it } from "node:test";
-import { parseUnits } from "viem";
+import assert from 'node:assert/strict';
+import { describe, it } from 'node:test';
+import { parseUnits } from 'viem';
 
-import { network } from "hardhat";
-import LPTokenModule from "../ignition/modules/LPToken.js";
-import { NetworkConnection } from "hardhat/types/network";
+import { network } from 'hardhat';
+import LPTokenModule from '../ignition/modules/LPToken.js';
+import { NetworkConnection } from 'hardhat/types/network';
+import { parse } from 'node:path';
 
 async function deploy(connection: NetworkConnection) {
   const { viem, ignition } = await network.connect();
@@ -19,57 +20,131 @@ async function deployWithPool(connection: NetworkConnection) {
   const { viem, ignition, publicClient, lpToken } = await deploy(connection);
   const [deployer, user] = await viem.getWalletClients();
 
-  const token0 = await viem.deployContract("MockERC20", ["Token0", "T0", parseUnits("1000000", 18)]);
-  const token1 = await viem.deployContract("MockERC20", ["Token1", "T1", parseUnits("1000000", 18)]);
+  const token0 = await viem.deployContract('MockERC20', [
+    'Token0',
+    'T0',
+    parseUnits('1000000', 18),
+  ]);
+  const token1 = await viem.deployContract('MockERC20', [
+    'Token1',
+    'T1',
+    parseUnits('1000000', 18),
+  ]);
 
+  await token0.write.approve([lpToken.address, parseUnits('100', 18)], {
+    account: deployer.account,
+  });
+  await token1.write.approve([lpToken.address, parseUnits('1000', 18)], {
+    account: deployer.account,
+  });
 
-  await token0.write.approve([lpToken.address, parseUnits("100", 18)], { account: deployer.account });
-  await token1.write.approve([lpToken.address, parseUnits("1000", 18)], { account: deployer.account });
-  
-  await token0.write.transfer([lpToken.address, parseUnits("100", 18)], { account: deployer.account });
-  await token1.write.transfer([lpToken.address, parseUnits("1000", 18)], { account: deployer.account });
+  await token0.write.transfer([lpToken.address, parseUnits('100', 18)], {
+    account: deployer.account,
+  });
+  await token1.write.transfer([lpToken.address, parseUnits('1000', 18)], {
+    account: deployer.account,
+  });
 
   await lpToken.write.initialize(
-    [token0.address, token1.address, parseUnits("100", 18), parseUnits("1000", 18)],
+    [
+      token0.address,
+      token1.address,
+      parseUnits('100', 18),
+      parseUnits('1000', 18),
+    ],
     { account: deployer.account }
   );
 
-  await token0.write.mint([user.account.address, parseUnits("100", 18)], { account: deployer.account });
-  await token1.write.mint([user.account.address, parseUnits("100", 18)], { account: deployer.account });
-
-  return { viem, ignition, publicClient, lpToken, token0, token1, user, deployer };
-}
-
-describe("LPToken", async function () {
-  it("Deploy ignition", async function () {
-    const { networkHelpers } = await network.connect();
-    const { lpToken } = await networkHelpers.loadFixture(deploy.bind(networkHelpers));
-
-    console.log("lpToken", lpToken.address);
+  await token0.write.mint([user.account.address, parseUnits('100', 18)], {
+    account: deployer.account,
+  });
+  await token1.write.mint([user.account.address, parseUnits('100', 18)], {
+    account: deployer.account,
   });
 
-  it("Swap amount out - nhận 100 T1, đổi T0", async function () {
+  return {
+    viem,
+    ignition,
+    publicClient,
+    lpToken,
+    token0,
+    token1,
+    user,
+    deployer,
+  };
+}
+
+describe('LPToken', async function () {
+  it('Deploy ignition', async function () {
     const { networkHelpers } = await network.connect();
-    const { token0, token1, lpToken, user, publicClient } = await networkHelpers.loadFixture(
-      deployWithPool.bind(networkHelpers)
+    const { lpToken } = await networkHelpers.loadFixture(
+      deploy.bind(networkHelpers)
     );
 
-    const amountOut = parseUnits("100", 18);
-    const amountInMax = parseUnits("15", 18);
+    console.log('lpToken', lpToken.address);
+  });
 
-    await token0.write.approve([lpToken.address, amountInMax], { account: user.account });
+  it('Swap amount in', async function () {
+    const { networkHelpers } = await network.connect();
+    const { token0, token1, lpToken, deployer, publicClient, viem } =
+      await networkHelpers.loadFixture(deployWithPool.bind(networkHelpers));
 
-    const balanceT1Before = (await token1.read.balanceOf([user.account.address])) as bigint;
+    const reserveIn = await lpToken.read.reserve0();
+    const reserveOut = await lpToken.read.reserve1();
 
-    const hash = await lpToken.write.swap_exact_out(
-      [amountOut, amountInMax, token1.address, user.account.address],
-      { account: user.account }
-    );
+    const amountInWithFee = 100n;
+    const amountInReal = (amountInWithFee * 997n) / 1000n;
+    const expectedAmountOut =
+      (amountInReal * reserveOut) / (reserveIn + amountInReal);
 
-    await publicClient.waitForTransactionReceipt({ hash });
+    await token0.write.approve([lpToken.address, amountInWithFee]);
 
-    const balanceT1After = (await token1.read.balanceOf([user.account.address])) as bigint;
+    const swapTask = lpToken.write.swap_amount_in([
+      amountInWithFee,
+      token0.address,
+      0n,
+    ]);
 
-    assert.equal(balanceT1After - balanceT1Before, amountOut, "User phải nhận đúng 100 T1");
+    await viem.assertions.erc20BalancesHaveChanged(swapTask, token0.address, [
+      { address: deployer.account.address, amount: -100n },
+      { address: lpToken.address, amount: 100n },
+    ]);
+
+    await viem.assertions.erc20BalancesHaveChanged(swapTask, token1.address, [
+      { address: deployer.account.address, amount: expectedAmountOut },
+      { address: lpToken.address, amount: -expectedAmountOut },
+    ]);
+  });
+
+  it('Swap amount out', async function () {
+    const { networkHelpers } = await network.connect();
+    const { token0, token1, lpToken, deployer, publicClient, viem } =
+      await networkHelpers.loadFixture(deployWithPool.bind(networkHelpers));
+
+    const amountOut = parseUnits('100', 18);
+    const amountInMax = parseUnits('15', 18);
+
+    await token0.write.approve([lpToken.address, amountInMax], {
+      account: deployer.account,
+    });
+
+    const reserveIn = await lpToken.read.reserve0();
+    const reserveOut = await lpToken.read.reserve1();
+
+    const expectedAmountIn =
+      (amountOut * reserveIn * 1000n) / ((reserveOut - amountOut) * 997n) + 1n;
+
+    await token0.write.approve([lpToken.address, expectedAmountIn]);
+
+    const swapTask = lpToken.write.swap_exact_out([
+      amountOut,
+      1000000000000000000000n,
+      token1.address,
+    ]);
+
+    await viem.assertions.erc20BalancesHaveChanged(swapTask, token0.address, [
+      { address: deployer.account.address, amount: -expectedAmountIn },
+      { address: lpToken.address, amount: expectedAmountIn },
+    ]);
   });
 });
